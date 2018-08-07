c2b907aabda9205e95e856caad4d26fb978c096e
Kyle J. Bilton <kjbilton@gmail.com>
2018-08-07 14:34:48 -0700

    Updated recommendations

--- FoodFlix/engine.py
+++ FoodFlix/engine.py
@@ -112,12 +112,21 @@ class FoodFlixEngine(object):
                                if_exists='replace')
         return
 
-    def predict(self, cals_per_day):
+    def predict(self, cals_per_day, w_like=1.5, w_dislike=0.3, n_closest=3):
         """
         Generate predictions
+
+        Parameters
+        ==========
+        cals_per_day : float
+            Total recommended calories per day for a user.
+        w_like : float
+            Weighting for liked recipes in computing recommendations.
+        w_dislike : float
+            Weighting for disliked recipes in computing recommendations.
         """
         # Divide the daily calories into five meals
-        cals_per_day /= 5 # 5 meals/day
+        cals_per_day /= 5
 
         # Connect to the database to grab user information
         db = get_db()
@@ -126,34 +135,74 @@ class FoodFlixEngine(object):
         liked = get_liked(session.get('user_id'))
         disliked = get_disliked(session.get('user_id'))
 
+        # The TF-IDF db uses recipe_id as the index as integers...
+        # TODO kjb: make all recipe_id indices either string or integer
+        liked = [int(l) for l in liked]
+        disliked = [int(l) for l in disliked]
+
         # Grab the TF-IDF features to compute scores
         tfidf_query = 'SELECT * FROM tfidf;'
         tfidf = pd.read_sql(sql=tfidf_query, con=db, index_col='recipe_id')
 
+        # Grab the feature vectors for the liked and disliked recipes
+        likes = tfidf.loc[liked]
+        dislikes = tfidf.loc[disliked]
+
+        # Compute the Rocchio topic
+        topic = self.compute_rocchio_topic(likes, dislikes, w_like, w_dislike)
+
+        # Find recipes similar to the Rocchio topic
+        similarity = cosine_similarity(np.atleast_2d(topic), tfidf)
+        similarity = pd.Series(similarity[0], index=tfidf.index)
+
+        recommendations = similarity.sort_values(ascending=False)
+        recommendations = list(recommendations.index)
+
         # Create a container to hold recommended recipes
         recipes = []
 
-        # Iterate over all of the recipes that a user has liked
-        for recipe in liked:
-            rec_query = db.execute(
+        n_recs = 0
+
+        for rec in recommendations:
+            recipe_query = db.execute(
                 'SELECT * '
-                'FROM recommendations '
-                'WHERE recipe_id == ?',
-                (recipe,)
+                'FROM recipes '
+                'WHERE recipe_id == ? ',
+                (rec,)
             ).fetchone()
 
-            # Skip the first one, since it's the item we are comparing to
-            for k in rec_query[1:]:
-                recipe_query = db.execute(
-                    'SELECT * '
-                    'FROM recipes '
-                    'WHERE recipe_id == ? ',
-                    (k,)
-                ).fetchone()
+            # Only recommend things that you don't already like
+            if recipe_query['recipe_id'] not in liked:
 
                 # Recommend only recipes around your cal/week goal
                 cals = int(recipe_query['calorie_count'].replace('cals',''))
-                if cals > cals_per_day-100 and cals < cals_per_day+100:
+                if cals > cals_per_day * 0.8 and cals < cals_per_day * 1.2:
                     recipes.append(recipe_query)
+                    n_recs+=1
+
+            if n_recs >= n_closest:
+                break
 
         return recipes
+
+    def compute_rocchio_topic(self, like, dislike, w_like, w_dislike):
+        """
+        Compute the Rocchio topic. This weights what the user likes and dislikes
+        to generate recommendations.
+        """
+        n_features = like.shape[1]
+
+        # Get the mean value of the liked recipes
+        topic_like = like.mean(axis=0)
+
+        # Account for the case where the user doesn't dislike anything
+        if len(dislike) == 0:
+            topic_dislike = np.zeros(n_features)
+        # Otherwise compute the mean of what is disliked
+        else:
+            topic_dislike = dislike.mean(axis=0)
+
+        # Compute the Rocchio topic
+        rocchio_topic = w_like * topic_like - w_dislike * topic_dislike
+
+        return rocchio_topic.values
